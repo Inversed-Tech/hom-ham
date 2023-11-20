17aa8b4a217d1b70a838d6b77822bdc8f05753e8
emmorais <eduardo.morais@gmail.com>
2023-11-20 09:26:40 -0300

    First implementation

---
+++ homham.py
@@ -0,0 +1,55 @@
+
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+from concrete import fhe
+
+configuration = fhe.Configuration(
+    enable_unsafe_features=True,
+    use_insecure_key_cache=True,
+    insecure_key_cache_location=".keys",
+
+    # To enable displaying progressbar
+    show_progress=True,
+    # To enable showing tags in the progressbar (does not work in notebooks)
+    progress_tag=True,
+    # To give a title to the progressbar
+    progress_title="Evaluation:",
+)
+
+SIZE = 1000
+
+@fhe.compiler({"bits": "encrypted"})
+def ham(bits):
+    total_sum = 0
+    for i in range(SIZE):
+        total_sum += (bits[i, 0] ^ bits[i, 1]) * bits[i, 2]
+    return total_sum
+
+data = np.random.randint(0, 2, (SIZE, 3))
+
+print()
+
+print(f"Compilation started @ {time.strftime('%H:%M:%S', time.localtime())}")
+start = time.time()
+inputset = [np.random.randint(0, 2, (SIZE, 3)) for _ in range(100)]
+circuit = ham.compile(inputset, configuration)
+end = time.time()
+print(f"(took {end - start:.3f} seconds)")
+
+print()
+
+print(f"Key generation started @ {time.strftime('%H:%M:%S', time.localtime())}")
+start = time.time()
+circuit.keygen()
+end = time.time()
+print(f"(took {end - start:.3f} seconds)")
+
+print()
+
+print(f"Evaluation started @ {time.strftime('%H:%M:%S', time.localtime())}")
+start = time.time()
+eval_data = circuit.encrypt_run_decrypt(data)
+end = time.time()
+print(f"(took {end - start:.3f} seconds)")
